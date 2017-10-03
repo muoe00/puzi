f42402345a0dda021f0bf366e8a11a64b61cbc36
uuuhy <uuuhy93@gmail.com>
2017-10-03 17:15:04 +0900

    채널 리스트

--- app/src/main/java/com/puzi/puzi/biz/company/CompanyVO.java
+++ app/src/main/java/com/puzi/puzi/biz/company/CompanyVO.java
@@ -8,41 +8,8 @@ import lombok.Data;
 
 @Data
 public class CompanyVO {
-
-	private int companyId;				// 회사시퀀스
-	private String companyAlias;		// 회사명
-	private String pictureUrl;			// 사진주소
-	private String comment;				// 설명
-
-	public int getCompanyId() {
-		return companyId;
-	}
-
-	public void setCompanyId(int companyId) {
-		this.companyId = companyId;
-	}
-
-	public String getCompanyAlias() {
-		return companyAlias;
-	}
-
-	public void setCompanyAlias(String companyAlias) {
-		this.companyAlias = companyAlias;
-	}
-
-	public String getPictureUrl() {
-		return pictureUrl;
-	}
-
-	public void setPictureUrl(String pictureUrl) {
-		this.pictureUrl = pictureUrl;
-	}
-
-	public String getComment() {
-		return comment;
-	}
-
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
+	private int companyId;
+	private String companyAlias;
+	private String pictureUrl;
+	private String comment;
 }
--- app/src/main/java/com/puzi/puzi/ui/channel/ChannelFragment.java
+++ app/src/main/java/com/puzi/puzi/ui/channel/ChannelFragment.java
@@ -5,92 +5,139 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.AbsListView;
 import android.widget.ListView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import com.puzi.puzi.R;
+import com.puzi.puzi.biz.channel.ChannelCategoryType;
+import com.puzi.puzi.biz.channel.ChannelEditorsPageVO;
 import com.puzi.puzi.biz.channel.ChannelVO;
 import com.puzi.puzi.cache.Preference;
+import com.puzi.puzi.network.CustomCallback;
+import com.puzi.puzi.network.ResponseVO;
 import com.puzi.puzi.network.RetrofitManager;
 import com.puzi.puzi.network.service.ChannelNetworkService;
+import retrofit2.Call;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by muoe0 on 2017-07-08.
  */
 
-public class ChannelFragment extends Fragment{
+public class ChannelFragment extends Fragment {
 
-	public static final int RECOMMEND = 0;
-	public static final int FAVORITS = 1;
-	private Button btnReccomend, btnFavorit;
-	private ListView lvChannel;
-	private List<ChannelVO> channelList;
-	private ChannelListAdapter channelListAdapter;
-	private int index = RECOMMEND;
-	private String type = "RECOMMEND";
+	private Unbinder unbinder;
+
+	private List<ChannelCategoryType> categoryTypeList = Arrays.asList(ChannelCategoryType.values());
+	private String searchType = null;
+	private int pagingIndex = 1;
+
+	private boolean more = true;
+	boolean lastestScrollFlag = false;
+
+	private ChannelListAdapter channelListAdapter = null;
+
+	@BindView(R.id.lv_channel) public ListView lvChannel;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		final View view = inflater.inflate(R.layout.fragment_channel, container, false);
-		initComponent(view);
-		getChannelList(view);
+		unbinder = ButterKnife.bind(this, view);
 
-//		btnReccomend.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				index = RECOMMEND;
-//				type = "RECOMMEND";
-//				getChannelList(view);
-//			}
-//		});
-//		btnFavorit.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				index = FAVORITS;
-//				type = "FAVORITES";
-//				getChannelList(view);
-//			}
-//		});
+		initScrollAction();
+
+		initAdapter();
+
+		getChannelWithEditorsPageList();
 
 		return view;
 	}
 
-	private void initComponent(View view) {
+	private void getChannelWithEditorsPageList() {
 
-		lvChannel = (ListView) view.findViewById(R.id.lv_channel);
-
-//		btnReccomend = (Button) view.findViewById(R.id.btn_channelRecommend);
-//		btnFavorit = (Button) view.findViewById(R.id.btn_channelFavorit);
-	}
-
-	public void getChannelList(final View view) {
+		channelListAdapter.startProgress();
+		lvChannel.setSelection(channelListAdapter.getCount() - 1);
 
 		final ChannelNetworkService channelNetworkService = RetrofitManager.create(ChannelNetworkService.class);
-
 		String token = Preference.getProperty(getActivity(), "token");
-		int pagingIndex = 1;
 
-//		Call<ResponseVO<List<ChannelVO>>> call = channelNetworkService.channelList(token, "", type, pagingIndex);
-//		call.enqueue(new CustomCallback<ResponseVO<List<ChannelVO>>>(getActivity()) {
-//			@Override
-//			public void onSuccess(ResponseVO<List<ChannelVO>> responseVO) {
-//				ResultType resultType = responseVO.getResultType();
-//
-//				if (resultType.isSuccess()) {
-//
-//					channelList = responseVO.getValue("channelList");
-//					Log.i("DEBUG", "ChannelFragment main / channelList : " + channelList.toString());
-//
-//					channelListAdapter = new ChannelListAdapter(view.getContext(), channelList);
-//					lvChannel.setAdapter(channelListAdapter);
-//				}
-//			}
-//		});
+		Call<ResponseVO> call = channelNetworkService.channelWithEditorsPageList(token, categoryTypeList, searchType, pagingIndex);
+		call.enqueue(new CustomCallback<ResponseVO>(getActivity()) {
+
+			@Override
+			public void onSuccess(ResponseVO responseVO) {
+
+				channelListAdapter.stopProgress();
+
+				if (responseVO.getResultType().isSuccess()) {
+					List<ChannelVO> newChannelList = responseVO.getList("channelDTOList", ChannelVO.class);
+
+					if(newChannelList.size() >= 4) {
+						List<ChannelEditorsPageVO> editorsPageList = responseVO.getList("channelEditorsPageDTOList", ChannelEditorsPageVO.class);
+
+						channelListAdapter.addChannel(newChannelList.subList(0, 4));
+						if(editorsPageList.size() >= 3) {
+							channelListAdapter.addEditorsPage(editorsPageList.subList(0, 3));
+						} else {
+							channelListAdapter.addEditorsPage(editorsPageList);
+						}
+						channelListAdapter.addChannel(newChannelList.subList(4, newChannelList.size()));
+						if(editorsPageList.size() >= 4) {
+							channelListAdapter.addEditorsPage(editorsPageList.subList(4, editorsPageList.size()));
+						}
+						channelListAdapter.notifyDataSetChanged();
+
+						searchType = responseVO.getString("searchType");
+					} else {
+						if(newChannelList.size() != 0) {
+							channelListAdapter.addChannel(newChannelList);
+							channelListAdapter.notifyDataSetChanged();
+						}
+						more = false;
+					}
+				}
+			}
+		});
 	}
+
+	private void initScrollAction() {
+		lvChannel.setOnScrollListener(new AbsListView.OnScrollListener() {
+
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+				if(scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE && lastestScrollFlag) {
+					if(more) {
+						pagingIndex = pagingIndex + 1;
+						getChannelWithEditorsPageList();
+					}
+				}
+			}
+
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+				lastestScrollFlag = (totalItemCount > 0) && firstVisibleItem + visibleItemCount >= totalItemCount;
+			}
+		});
+	}
+
+	private void initAdapter() {
+		channelListAdapter = new ChannelListAdapter(getActivity());
+		lvChannel.setAdapter(channelListAdapter);
+	}
+
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		unbinder.unbind();
+	}
+
 }
