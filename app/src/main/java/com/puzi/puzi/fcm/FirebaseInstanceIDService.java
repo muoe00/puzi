c46493005c76bda77cd0a1ab504000642df34fb3
uuuhy <uuuhy93@gmail.com>
2017-10-17 23:40:19 +0900

    171017 jh layout modify commit

--- app/src/main/java/com/puzi/puzi/fcm/FirebaseInstanceIDService.java
+++ app/src/main/java/com/puzi/puzi/fcm/FirebaseInstanceIDService.java
@@ -17,27 +17,13 @@ public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
 
 	@Override
 	public void onTokenRefresh() {
-		// Get updated InstanceID token.
 		String refreshedToken = FirebaseInstanceId.getInstance().getToken();
 		Log.i(PuziUtils.INFO, "Refreshed token: " + refreshedToken);
 
-		// If you want to send messages to this application instance or
-		// manage this apps subscriptions on the server side, send the
-		// Instance ID token to your app server.
 		sendRegistrationToServer(refreshedToken);
 	}
-	// [END refresh_token]
 
-	/**
-	 * Persist token to third-party servers.
-	 *
-	 * Modify this method to associate the user's FCM InstanceID token with any server-side account
-	 * maintained by your application.
-	 *
-	 * @param token The new token.
-	 */
 	private void sendRegistrationToServer(String token) {
 		// Add custom implementation, as needed.
-
 	}
 }
--- app/src/main/java/com/puzi/puzi/fcm/FirebaseMessagingService.java
+++ app/src/main/java/com/puzi/puzi/fcm/FirebaseMessagingService.java
@@ -24,64 +24,7 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
 
 	@Override
 	public void onMessageReceived(RemoteMessage remoteMessage) {
-		// [START_EXCLUDE]
-		// There are two types of messages data messages and notification messages. Data messages are handled
-		// here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
-		// traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
-		// is in the foreground. When the app is in the background an automatically generated notification is displayed.
-		// When the user taps on the notification they are returned to the app. Messages containing both notification
-		// and data payloads are treated as notification messages. The Firebase console always sends notification
-		// messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-		// [END_EXCLUDE]
-
 		sendPushNotification(remoteMessage.getData().get("message"));
-
-		/*// TODO(developer): Handle FCM messages here.
-		// Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-		Log.i(PuziUtils.INFO, "From: " + remoteMessage.getFrom());
-
-		// Check if message contains a data payload.
-		if (remoteMessage.getData().size() > 0) {
-			Log.i(PuziUtils.INFO, "Message data payload: " + remoteMessage.getData());
-		}
-
-		// Check if message contains a notification payload.
-		if (remoteMessage.getNotification() != null) {
-			Log.i(PuziUtils.INFO, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-		}
-
-		msg = remoteMessage.getNotification().getBody();
-
-		// Also if you intend on generating your own notifications as a result of a received FCM
-		// message, here is where that should be initiated. See sendNotification method below.
-
-		// [END receive_message]
-
-		Intent intent = new Intent(this, IntroActivity.class);
-		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-			new Intent(this, IntroActivity.class), PendingIntent.FLAG_ONE_SHOT);
-
-		NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this).setSmallIcon(R.mipmap.splash_logo)
-			.setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.splash_logo))
-			.setContentTitle("Puzi messaging test")
-			.setContentText(msg)
-			.setAutoCancel(true)
-			.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-			.setVibrate(new long[]{1, 1000})
-			.setLights(000000255,500,2000)
-			.setContentIntent(contentIntent);;
-
-		NotificationManager notificationManager =
-			(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-		PowerManager pm = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
-		PowerManager.WakeLock wakelock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
-		wakelock.acquire(5000);
-
-		notificationManager.notify(0 *//* ID of notification *//*, mBuilder.build());
-
-		mBuilder.setContentIntent(contentIntent);*/
 	}
 
 	private void sendPushNotification(String message) {
