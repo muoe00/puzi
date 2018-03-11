1862f4e70d11c99561f99b48820dd0d3ddb2c02e
uuuhy <uuuhy93@gmail.com>
2018-03-11 19:03:03 +0900

    Retrofit url gitignore 적용

---
+++ app/src/main/java/kr/puzi/puzi/network/FixValues.java
@@ -0,0 +1,10 @@
+package kr.puzi.puzi.network;
+
+/**
+ * Created by juhyun on 2018. 3. 11..
+ */
+
+public class FixValues {
+
+    public static final String baseURL = "https://api.puzi.kr";
+}
--- app/src/main/java/kr/puzi/puzi/network/RetrofitManager.java
+++ app/src/main/java/kr/puzi/puzi/network/RetrofitManager.java
@@ -9,9 +9,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitManager {
 
-    private static final String baseURL_DEBUG = "http://dev.api.puzi.kr";
-    private static final String baseURL = "https://api.puzi.kr";
-
 	public static <T> T create(Class<T> c){
 
 		return new Retrofit.Builder()
